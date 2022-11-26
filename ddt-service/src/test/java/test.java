135b3de07585b4ccb93110b00a00780a904bf483
duanmt <824257015@qq.com>
2022-11-26 11:07:02 +0800

    feat: 补充项目结构，新增拦截器日志打印

--- ddt-service/src/test/java/test.java
+++ ddt-service/src/test/java/test.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+
 /**
  * @Author Diuut
  * @Date 2020/9/8  16:32
@@ -21,6 +23,28 @@ public class test {
        String test="123456";
         System.out.println(test.indexOf("87"));
     }
+
+    @Test
+    public void DreamPoint() {
+        String[] redList={"01","02","03","04","05","06","07"};
+        String[] blueList={"10","11"};
+        String[] newList={};
+        String blueBall="";
+        for (int i = 0; i < blueList.length; i++) {
+            blueBall=blueList[i];
+            StringBuilder redBall=new StringBuilder();
+            for (int x = 0; x < redList.length; x++) {
+                System.out.println();
+            }
+        }
+    }
+
+    private void pointDD(String[] list){
+        if (list!=null) {
+
+        }
+    }
+
 
 
 }
